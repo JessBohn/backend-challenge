9f104d35c433feffbf62de89b8b69a98ed912605
Jessica Bohn <bohn.16@wright.edu>
2021-02-08 00:01:26 -0600

    Create show page for Member profile

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -3,4 +3,8 @@ class MembersController < ApplicationController
     # The index will return all members if a value is not provided by params
     @members = Member.search(params[:query])
   end
+
+  def show
+    @member = Member.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root "members#index"
 
+  # Could use resources to create all CRUD routes for members, but we do not need edit/update or delete
   get '/members', to: "members#index"
+  get '/members/:id', to: "members#show", as: 'member'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
