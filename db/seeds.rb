112110d82437f04e0d64d6be4a58ea61d11e24a2
Jessica Bohn <bohn.16@wright.edu>
2021-02-08 01:59:20 -0600

    Add DB seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Member.create([{name: "Jessica Bohn", website: "https://www.everlywell.com/blog/categories/news-and-info/"},
+                {name: "Sasha Fierce", website: "https://news.nike.com/"},
+                {name: "Jimmy Choo", website: "https://www.theshoesnobblog.com/"},
+                {name: "Shawn Murray", website: "https://www.nasa.gov/news/releases/latest/"}])
+
+Friendship.create([{member_id: 1, friend_id: 4}, {member_id: 2, friend_id: 3}, {member_id: 1, friend_id: 2}])
