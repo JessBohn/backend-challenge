d1a37237c7414c3b5548e1630721e9a237a7d792
Jessica Bohn <bohn.16@wright.edu>
2021-02-07 22:59:00 -0600

    Setup initial search function for Member

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,2 +1,18 @@
 class Member < ApplicationRecord
+  # Tried using postgres/ACtiveRecord array query using ANY? but only works with exact matches
+  # The below method works, but is overly complex and makes too many calls
+  # Will refactor this after tackling other requirements
+  def self.search(search)
+    members = []
+    if search.present?
+      Member.all.each do |member|
+        if member.tags.any?(/#{search}/)
+          members << member
+        end
+      end
+    else
+      members = find(:all)
+    end
+    members
+  end
 end
